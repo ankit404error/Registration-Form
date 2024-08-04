c757d46bd8bc95fd173f141b3007b5cd38075e4e
Ankit <pandeyankit54562@gmail.com>
2024-08-04 21:50:23 +0530

    commit 1

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+
+const uri = `mongodb+srv://ankit404error:${password}@cluster0.n6pf4wn.mongodb.net/registrationFormDB`;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch((error) => {
+    console.log("Error connecting to the database:", error);
+});
+
+// registration schema
+const registrationSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+// model
+const Registration = mongoose.model("Registration", registrationSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/register", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUSER = await Registration.findOne({email : email});
+
+        if(!existingUSER){
+        const registrationData = new Registration({
+            name,
+            email,
+            password
+        });
+        await registrationData.save();
+        res.redirect("/success");
+    }
+    else{
+        console.log("user exist");
+    }
+
+    } catch (error) {
+        console.log(error);
+        res.redirect("/error");
+    }
+});
+
+app.get("/success", (req, res) => {
+    res.sendFile(__dirname + "/success.html");
+});
+
+app.get("/error", (req, res) => {
+    res.sendFile(__dirname + "/error.html");
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening to port ${port}`);
+});
